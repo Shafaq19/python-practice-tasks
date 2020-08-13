2b3c5ebb19f633034c93fcd28643bd6e59bcc82b
Shafaq Arshad <arshad.shafaq15@gmail.com>
2020-08-13 17:00:04 +0500

    feat: function which can compute the sum of two provided numbers.

---
+++ PythonTask1.py
@@ -0,0 +1,10 @@
+def Summ(num1,num2):
+    """
+    function which can compute the sum of two provided numbers.
+    :param num1:
+    :param num2:
+    :return:
+    """
+    return num1+num2
+a,b=10,20
+print("sum of %d and %d is %d"%(a,b,Summ(a,b)))
